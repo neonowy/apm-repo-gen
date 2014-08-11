0d74965d7996e445dafcbeb5e9d9b291b8c8bee3
neonowy <krzysztof.zbudniewek@outlook.com>
2014-08-11 20:51:08 +0200

    Improved tests.

--- test/test_ApmRepoGen.rb
+++ test/test_ApmRepoGen.rb
@@ -3,22 +3,32 @@ require "test/unit"
 require "json"
 
 class TestApmRepoGen < Test::Unit::TestCase
-	@@packages = ApmRepoGen::Packages.new(File.join(Dir.pwd, "test", "tmp"))
+	@@pkgs = ApmRepoGen::Packages.new(File.join(Dir.pwd, "test", "tmp"))
 
 	def test_packages
-		assert_equal(["a", "abc"], @@packages.packages)
+		assert_block do
+			if @@pkgs.packages == ["abc", "a"] || @@pkgs.packages == ["a", "abc"]
+				return true
+			else
+				return false
+			end
+		end
 	end
 
 	def test_packages_length
-		#packages = Packages.new
-
-		assert_equal(2, @@packages.length)
+		assert_equal(2, @@pkgs.length)
 	end
 
 	def test_packages_writing
-		#packages = Packages.new
-		@@packages.write
+		@@pkgs.write
 		file = open(File.join(Dir.pwd, "test", "tmp", "packages.json"))
-		assert_equal(["a", "abc"].to_json, file.read)
+		json = file.read
+		assert_block do
+			if json == ["abc", "a"].to_json || json == ["a", "abc"].to_json
+				return true
+			else
+				return false
+			end
+		end
 	end
 end
