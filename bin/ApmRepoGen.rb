7ab4b14680e68f5f4f56badd854c458d9b6ab5d3
neonowy <krzysztof.zbudniewek@outlook.com>
2014-08-11 17:35:08 +0200

    Removed Coveralls support and changed bin/ApmRepoGen.rb

--- bin/ApmRepoGen.rb
+++ bin/ApmRepoGen.rb
@@ -1,5 +1,3 @@
 require_relative "../lib/ApmRepoGen"
 
-puts ARGV
-puts ARGV.length
 ApmRepoGen::Application.new(ARGV).run
--- test/test_ApmRepoGen.rb
+++ test/test_ApmRepoGen.rb
@@ -1,5 +1,3 @@
-require "coveralls"
-Coveralls.wear!
 require "ApmRepoGen/packages.rb"
 require "test/unit"
 require "json"
